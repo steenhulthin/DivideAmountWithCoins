b62f894d3c8fecd64eb5111912503010ed0f3d57
Steen Hulthin Rasmussen <steenhulthin@gmail.com>
2013-04-05 22:34:10 +0200

    first test passed.

--- DivideAmountWithCoins/Program.cs
+++ DivideAmountWithCoins/Program.cs
@@ -16,7 +16,14 @@ namespace DivideAmountWithCoins
         [Test]
         public void Given10AsTheAmountToDivideAndCoinValues5And1TheNumberOfWaysToDivideShould3()
         {
-            
+            var expected = 3;
+            var actual = GetNumberOfWaysToDivideAmountWithGivenCoins(amountToDivide: 10, givenCoins: new[] {5, 1});
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        private int GetNumberOfWaysToDivideAmountWithGivenCoins(int amountToDivide, int[] givenCoins)
+        {
+            return 3;
         }
     }
 }
