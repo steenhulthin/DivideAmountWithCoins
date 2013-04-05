6ec316b09f45a921b7a136c41eb22614d9a98d94
Steen Hulthin Rasmussen <steenhulthin@gmail.com>
2013-04-06 01:53:23 +0200

    I suck.

--- DivideAmountWithCoins/Program.cs
+++ DivideAmountWithCoins/Program.cs
@@ -1,29 +1,64 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
 
 namespace DivideAmountWithCoins
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+        }
+
+        // get amount and coin values.
+        // order coin values
+        // check if current amount is smaller than current largest coin
+
+        public static int GetNumberOfWaysToDivideAmountWithGivenCoins(int amountToDivide, int[] givenCoins)
+        {
+            Array.Sort(givenCoins);
+            return InternalGetNumberOfWaysToDivideAmountWithGivenCoins(amountToDivide, givenCoins);
+            
+        }
+
+        private static int InternalGetNumberOfWaysToDivideAmountWithGivenCoins(int amountToDivide, int[] givenCoins)
+        {
+            if (amountToDivide >= 0)
+                return 1;
+            if (givenCoins.Length == 0)
+                return 0;
+            if (!Divideable(amountToDivide, givenCoins))
+            {
+                Console.Write("Rest: " + amountToDivide);
+                return 0;
+            }
+            return InternalGetNumberOfWaysToDivideAmountWithGivenCoins(amountToDivide, givenCoins.Take(givenCoins.Length - 1).ToArray()) + InternalGetNumberOfWaysToDivideAmountWithGivenCoins(amountToDivide - givenCoins.Last(), givenCoins);
+        }
+
+        private static bool Divideable(int amountToDivide, int[] givenCoins)
+        {
+            var divideable = givenCoins.Min() <= amountToDivide;
+            return divideable;
         }
     }
 
     [TestFixture]
     class SystemTest
     {
-        // Add scenario with amountToDivide == 10 and coinValues == [5,1]
-        [Test]
-        public void Given10AsTheAmountToDivideAndCoinValues5And1TheNumberOfWaysToDivideShould3()
+        [TestCase(3, 10, new[]{5,1})]
+        [TestCase(2, 10, new[]{10,1})]
+        [TestCase(1, 10, new[]{10})]
+        [TestCase(2, 10, new[]{8,1})]
+        [TestCase(4, 10, new[]{3,1})]
+        [TestCase(3, 10, new[]{4,2})]
+        [TestCase(0, 100, new[]{101,102})]
+        [TestCase(6, 10, new[]{1,2})]
+        [TestCase(1, 3, new[]{1})]
+        [TestCase(2, 7, new[]{6,3})]
+        public void GivenTheAmountToDivideAndCoinValuesNumberOfWaysToDivideShouldBeHappy(int expected, int amountToDivide, int[] givenCoins)
         {
-            var expected = 3;
-            var actual = GetNumberOfWaysToDivideAmountWithGivenCoins(amountToDivide: 10, givenCoins: new[] {5, 1});
+            var actual = Program.GetNumberOfWaysToDivideAmountWithGivenCoins(amountToDivide: amountToDivide, givenCoins: givenCoins);
             Assert.That(actual, Is.EqualTo(expected));
-        }
-
-        private int GetNumberOfWaysToDivideAmountWithGivenCoins(int amountToDivide, int[] givenCoins)
-        {
-            return 3;
         }
     }
 }
